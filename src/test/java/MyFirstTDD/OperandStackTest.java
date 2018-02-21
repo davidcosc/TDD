5cf2ab9f81247182d9e818002ba2a23f15e48c0f
david <d.cornel@tu-braunschweig.de>
2018-02-21 09:59:25 +0100

    Refactored OperandStackTest to create new OperandStack in @Before.

--- src/test/java/MyFirstTDD/OperandStackTest.java
+++ src/test/java/MyFirstTDD/OperandStackTest.java
@@ -1,5 +1,6 @@
 package MyFirstTDD;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -10,17 +11,27 @@ import static org.junit.Assert.assertEquals;
  * Created by david on 21.02.2018.
  */
 public class OperandStackTest {
+    OperandStack values;
+
+    @Before
+    public void setUpNewOperandStack() throws Exception {
+        values = new OperandStack();
+    }
+
     @Test
     public void testNewStackPeekIsZero() throws Exception {
-        OperandStack values = new OperandStack();
         assertEquals(BigDecimal.ZERO, values.getPeek());
     }
 
     @Test
     public void testPushValueOntoStack() throws Exception {
-        OperandStack values = new OperandStack();
         BigDecimal value = new BigDecimal(15);
         values.push(value);
         assertEquals(value, values.getPeek());
+    }
+
+    @Test
+    public void testReplacePeekValueOfStack() throws Exception {
+
     }
 }
