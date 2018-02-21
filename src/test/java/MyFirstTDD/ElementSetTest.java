55aa80ae8cd7a123d5b6da7c8a11b9c3f92acd84
david <d.cornel@tu-braunschweig.de>
2018-02-21 23:11:05 +0100

    ElementSet isEmpty method run by red green refactor.

--- src/test/java/MyFirstTDD/ElementSetTest.java
+++ src/test/java/MyFirstTDD/ElementSetTest.java
@@ -1,8 +1,14 @@
 package MyFirstTDD;
 
+import org.junit.Test;
+
 /**
  * Created by david on 21.02.2018.
  */
 public class ElementSetTest {
-    
+    @Test
+    public void testElementSetIsEmpty() throws Exception {
+        ElementSet elements = new ElementSet();
+        assert elements.isEmpty() == true;
+    }
 }
