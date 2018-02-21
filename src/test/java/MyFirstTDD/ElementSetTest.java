dba783a80ed53f4e7e93d91e72b32738ed13170c
david <d.cornel@tu-braunschweig.de>
2018-02-21 23:36:32 +0100

    Another reset.

--- src/test/java/MyFirstTDD/ElementSetTest.java
+++ src/test/java/MyFirstTDD/ElementSetTest.java
@@ -6,15 +6,5 @@ import org.junit.Test;
  * Created by david on 21.02.2018.
  */
 public class ElementSetTest {
-    @Test
-    public void testElementSetIsEmptyOnEmptySet() throws Exception {
-        ElementSet elements = new ElementSet();
-        assert elements.isEmpty() == true;
-    }
-
-    @Test
-    public void testElementSetSizeOnEmptySet() throws Exception {
-        ElementSet elements = new ElementSet();
-        assert elements.size() == 0;
-    }
+   
 }
