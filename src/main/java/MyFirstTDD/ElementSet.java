c4c7fb6d3e9f735a2f7d7efd6d8d2a0f588a5594
david <d.cornel@tu-braunschweig.de>
2018-02-21 23:39:26 +0100

    Implemented testIsEmptyOnEmptySet after red green refactor.

---
+++ src/main/java/MyFirstTDD/ElementSet.java
@@ -0,0 +1,10 @@
+package MyFirstTDD;
+
+/**
+ * Created by david on 21.02.2018.
+ */
+public class ElementSet {
+    public boolean isEmpty() {
+        return true;
+    }
+}
--- src/test/java/MyFirstTDD/ElementSetTest.java
+++ src/test/java/MyFirstTDD/ElementSetTest.java
@@ -6,5 +6,9 @@ import org.junit.Test;
  * Created by david on 21.02.2018.
  */
 public class ElementSetTest {
-
+    @Test
+    public void testIsEmptyOnEmptySet() throws Exception {
+        ElementSet elements = new ElementSet();
+        assert elements.isEmpty() == true;
+    }
 }
