760cef0a6560bb1bf3cafbe3248bf45568528b70
david <d.cornel@tu-braunschweig.de>
2018-02-21 23:45:04 +0100

    Implemented testIsEmptyOnFilledSet after red green. Refactored ElementSet creation to setUpElementSet method.

--- src/main/java/MyFirstTDD/ElementSet.java
+++ src/main/java/MyFirstTDD/ElementSet.java
@@ -4,7 +4,13 @@ package MyFirstTDD;
  * Created by david on 21.02.2018.
  */
 public class ElementSet {
+    int size = 0;
+
     public boolean isEmpty() {
-        return true;
+        return (size == 0);
+    }
+
+    public void add() {
+        size++;
     }
 }
--- src/test/java/MyFirstTDD/ElementSetTest.java
+++ src/test/java/MyFirstTDD/ElementSetTest.java
@@ -1,14 +1,26 @@
 package MyFirstTDD;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by david on 21.02.2018.
  */
 public class ElementSetTest {
+    ElementSet elements;
+    @Before
+    public void setUpElementSet() throws Exception {
+        elements = new ElementSet();
+    }
+
     @Test
     public void testIsEmptyOnEmptySet() throws Exception {
-        ElementSet elements = new ElementSet();
         assert elements.isEmpty() == true;
+    }
+
+    @Test
+    public void testIsEmptyOnFilledSet() throws Exception {
+        elements.add();
+        assert elements.isEmpty() == false;
     }
 }
